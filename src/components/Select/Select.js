b6bec3f496640b2f1e99621a721afd2ef66fbd42
Katrin Fritz <9885028+katrinundfritz@users.noreply.github.com>
2021-05-16 13:54:47 +0200

    Add select

--- src/components/Select/Select.js
+++ src/components/Select/Select.js
@@ -8,11 +8,50 @@ import { getDisplayedValue } from './Select.helpers';
 const Select = ({ label, value, onChange, children }) => {
   const displayedValue = getDisplayedValue(value, children);
 
-  return (
-    <select value={value} onChange={onChange}>
+  return (<Wrapper>
+    <NativeSelect value={value} onChange={onChange}>
       {children}
-    </select>
-  );
+    </NativeSelect>
+    <PresentationalBit>
+      {displayedValue}
+      <Icon id='chevron-down' strokeWidth={1} size={24} style={{marginLeft: '24px'}} />
+    </PresentationalBit>
+  </Wrapper>);
 };
+
+const Wrapper = styled.div`
+  position: relative;
+  width: max-content;
+`
+
+const NativeSelect = styled.select`
+  position: absolute;
+  top: 0;
+  left: 0;
+  height: 100%;
+  width: 100%;
+  opacity: 0;
+`
+
+const PresentationalBit = styled.div`
+  background-color: ${COLORS.transparentGray15};
+  border-radius: 8px;
+  height: 43px;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  padding: 12px 10px 12px 16px;
+  font-size: ${16 / 16}rem;
+  color: ${COLORS.gray700};
+
+  ${NativeSelect}:focus + & {
+    outline: 1px dotted #212121;
+    outline: 50px auto -webkit-focus-ring-color;
+  }
+
+  ${NativeSelect}:hover + & {
+    color: ${COLORS.black};
+  }
+`
 
 export default Select;
